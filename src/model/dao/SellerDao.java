38a3afba788debb754f6527910d35df191e89613
rrmcastro <ricardormcastro@gmail.com>
2020-07-28 18:54:06 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,8 +7,13 @@ import model.entities.Seller;
 public interface SellerDao {
 
 	void insert(Seller obj);
+
 	void update(Seller obj);
+
 	void deleteById(Integer id);
+
 	Seller findById(Integer id);
+
 	List<Seller> findAll();
+
 }
