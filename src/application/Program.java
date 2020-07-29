db5c8145876bf0c223003627aee2e07ba57df784
rrmcastro <ricardormcastro@gmail.com>
2020-07-28 23:29:29 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,10 +16,16 @@ public class Program {
 		System.out.println("==== TESTE 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n==== TESTE 2: seller findByDepartment ====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n==== TESTE 3: seller findAll ====");
+		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
